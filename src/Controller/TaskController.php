776670f1544af6ae5fc6afe646a7197ae6601611
Magali <magali.thuaire@gmail.com>
2022-05-27 18:17:24 +0200

    🔧 [CONFIG] Updated bundle doctrine

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -15,7 +15,7 @@ class TaskController extends AbstractController
      */
     public function listAction()
     {
-        return $this->render('task/list.html.twig', ['tasks' => $this->getDoctrine()->getRepository('App:Task')->findAll()]);
+        return $this->render('task/list.html.twig', ['tasks' => $this->getDoctrine()->getRepository(Task::class)->findAll()]);
     }
 
     /**
