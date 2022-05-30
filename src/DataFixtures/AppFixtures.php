f7ae1d91de674e2f80055777f14ba795834d6996
Magali <magali.thuaire@gmail.com>
2022-05-30 21:29:05 +0200

    ✨ [BACK] Added data fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\TaskFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +11,18 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Users
+        UserFactory::new()
+           ->withAttributes([
+               'email' => 'magali@todolist.fr',
+               'username' => 'magali',
+               'password' => 'todolist',
+           ])
+           ->create();
 
-        $manager->flush();
+        UserFactory::createMany(10);
+
+        // Tasks
+        TaskFactory::createMany(20);
     }
 }
---
+++ src/Factory/TaskFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Task;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<Task>
+ *
+ * @method static Task|Proxy createOne(array $attributes = [])
+ * @method static Task[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static Task|Proxy find(object|array|mixed $criteria)
+ * @method static Task|Proxy findOrCreate(array $attributes)
+ * @method static Task|Proxy first(string $sortedField = 'id')
+ * @method static Task|Proxy last(string $sortedField = 'id')
+ * @method static Task|Proxy random(array $attributes = [])
+ * @method static Task|Proxy randomOrCreate(array $attributes = [])
+ * @method static Task[]|Proxy[] all()
+ * @method static Task[]|Proxy[] findBy(array $attributes)
+ * @method static Task[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Task[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method Task|Proxy create(array|callable $attributes = [])
+ */
+final class TaskFactory extends ModelFactory
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        // TODO inject services if required (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services)
+    }
+
+    protected function getDefaults(): array
+    {
+        return [
+            'createdAt' =>  self::faker()->dateTimeBetween('-30 days', '-15 days'),
+            'title' => self::faker()->text(20),
+            'content' => self::faker()->paragraph(3),
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+        return $this
+            ->afterInstantiate(function (Task $task): void {
+                if (self::faker()->boolean()) {
+                    $task->toggle(true);
+                }
+            });
+    }
+
+    protected static function getClass(): string
+    {
+        return Task::class;
+    }
+}
